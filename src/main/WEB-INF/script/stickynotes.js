291d7a69920445e3bd8d26f888cb2aa63005f3f7
Prakruthi Nagaraj <prakruthi28@gmail.com>
2014-11-29 15:37:27 -0800

    Added JSON ajax request/response

--- src/main/WEB-INF/script/stickynotes.js
+++ src/main/WEB-INF/script/stickynotes.js
@@ -2,18 +2,23 @@
                                      
 jQuery(document).ready(function() {
 var userdata = { "userid": "admin"};
-
+var docdata = { "name":"new-document",
+				"message": "empty data"}
 
 $("#new_note").click(function() {
 	var methodURL = "http://localhost:8080/json/ajaxJsonRequest/12345/createnote";
+	//alert(JSON.stringify(docdata));
 	$.ajax ({
 		// change the type to match controller 
 		type : "POST",
+		contentType: 'application/json',
+		dataType : "json",
 		url : methodURL,
-		data : JSON.stringify(userdata),
+		data : JSON.stringify(docdata),
 		success : function(data) {
-			// Write code here to perform anything with data.
-            alert(data)
+		 	// Write code here to perform anything with data.
+            alert(data.id)
+			alert(data.doc)
 		},
 		errror: function(e) {
 		}
